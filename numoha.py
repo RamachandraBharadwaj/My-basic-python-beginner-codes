e6f2ada454c9596fe2a1d3bc9cf1e6e9c66499e3
Ch.R.Bharadwaj <131685680+RamachandraBharadwaj@users.noreply.github.com>
2023-06-16 20:00:47 +0530

    Converting a number to it's word form (English)
    Note-Limit up to only 4 digits!!
    
    Honestly, I'm happy that I figured this out my own without asking anyone or googling it. I made errors but I've completed this in 45 minutes approximately. (time not consecutive). I know this is a a bit inefficient code but I'm reasonably happy about this

---
+++ numoha.py
@@ -0,0 +1,61 @@
+def convo(x):
+    c=0
+    global l
+    global n
+    re=0
+    word=""
+    if(l<=4):
+        while(x!=0):
+            te=""
+            z=re
+            re=x%10
+            if(re==1 and c==1):
+                te=spl[z]
+                word=""
+                word=te+" "+word
+                c+=1
+                x=x//10
+                continue
+
+            if(re==0):
+                c+=1
+                x=x//10
+                continue
+            if(c!=1):
+                te=numbers[0][re]
+                if(c==2):
+                    te=te+" hundred "
+                if(c==3):
+                    te=te+" thousand "
+                word=te+word
+            else:
+                te=numbers[1][re]
+                word=te+" "+word
+            c+=1
+            x=x//10
+        print(word)
+    else:
+        print("Length more than 4 is not supported")
+            
+    
+    
+n=input("Enter a number:")
+l=len(n)
+numbers=[["","one","two","three","four","five","six","seven","eight","nine"],["","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninty"]]
+spl=["","eleven","twelve","thierteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+try:
+    x=int(n)
+    match x:
+        case 0:
+            print("zero")
+        case 10:
+            print("ten")
+        case 100:
+            print("hundred")
+        case 1000:
+            print("thousand")
+        case _:
+            convo(x)
+
+except:
+    print("Invalid input")
